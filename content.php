7e8865173d30125cb45468a39616fb6fd833e35a
Victorien Schweitzer <gaibe--ct--@live.fr>
2015-04-12 17:09:15 +0200

    amelioration du rendu des produit

--- content.php
+++ content.php
@@ -1,234 +1,36 @@
 <div class="container" id="main">
-<div class="row">
-   <div class="col-md-4 col-sm-6">
-        <div class="panel panel-default">
-          <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Bootstrap Examples</h4></div>
-            <div class="panel-body">
-              <div class="list-group">
-                <a href="http://bootply.com/tagged/modal" class="list-group-item">Modal / Dialog</a>
-                <a href="http://bootply.com/tagged/datetime" class="list-group-item">Datetime Examples</a>
-                <a href="http://bootply.com/tagged/datatable" class="list-group-item">Data Grids</a>
-              </div>
-            </div>
-        </div>
-        <div class="well"> 
-             <form class="form-horizontal" role="form">
-              <h4>What's New</h4>
-               <div class="form-group" style="padding:14px;">
-                <textarea class="form-control" placeholder="Update your status"></textarea>
-              </div>
-              <button class="btn btn-success pull-right" type="button">Post</button><ul class="list-inline"><li><a href="#"><i class="glyphicon glyphicon-align-left"></i></a></li><li><a href="#"><i class="glyphicon glyphicon-align-center"></i></a></li><li><a href="#"><i class="glyphicon glyphicon-align-right"></i></a></li></ul>
-            </form>
-        </div>
-     
-        <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>More Templates</h4></div>
-            <div class="panel-body">
-              <img src="//placehold.it/150x150" class="img-circle pull-right"> <a href="#">Free @Bootply</a>
-              <div class="clearfix"></div>
-              There a load of new free Bootstrap 3 ready templates at Bootply. All of these templates are free and don't require extensive customization to the Bootstrap baseline.
-              <hr>
-              <ul class="list-unstyled"><li><a href="http://www.bootply.com/templates">Dashboard</a></li><li><a href="http://www.bootply.com/templates">Darkside</a></li><li><a href="http://www.bootply.com/templates">Greenfield</a></li></ul>
-            </div>
-         </div> 
+    <div class="row">
 
-    </div>
-    <div class="col-md-4 col-sm-6">
-         
-          <div class="well"> 
-             <form class="form">
-              <h4>Sign-up</h4>
-              <div class="input-group text-center">
-              <input type="text" class="form-control input-lg" placeholder="Enter your email address">
-                <span class="input-group-btn"><button class="btn btn-lg btn-primary" type="button">OK</button></span>
-              </div>
-            </form>
-          </div>
+    <?php
+    for ($i = 0; $i < 12; $i++) {
 
-         <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Bootply Editor &amp; Code Library</h4></div>
-            <div class="panel-body">
-              <p><img src="//placehold.it/150x150" class="img-circle pull-right"> <a href="#">The Bootstrap Playground</a></p>
-              <div class="clearfix"></div>
-              <hr>
-              Design, build, test, and prototype using Bootstrap in real-time from your Web browser. Bootply combines the power of hand-coded HTML, CSS and JavaScript with the benefits of responsive design using Bootstrap. Find and showcase Bootstrap-ready snippets in the 100% free Bootply.com code repository.
-            </div>
-         </div>
+    ?>
       
-         <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Portlet Heading</h4></div>
-            <div class="panel-body">
-              <ul class="list-group">
-              <li class="list-group-item">Modals</li>
-              <li class="list-group-item">Sliders / Carousel</li>
-              <li class="list-group-item">Thumbnails</li>
-              </ul>
-            </div>
-         </div>
-      
-    </div>
     <div class="col-md-4 col-sm-6">
-         <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Portlet Heading</h4></div>
-            <div class="panel-body">
-              <ul class="list-group">
-              <li class="list-group-item">Bootply Playground</li>
-              <li class="list-group-item">Bootstrap Editor</li>
-              <li class="list-group-item">Bootstrap Templates</li>
-              </ul>
-            </div>
-        </div>
-        
-        <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Stackoverflow</h4></div>
-            <div class="panel-body">
-              <img src="//placehold.it/150x150" class="img-circle pull-right"> <a href="#">Keyword: Bootstrap</a>
-              <div class="clearfix"></div>
-              <hr>
-              
-              <p>If you're looking for help with Bootstrap code, the <code>twitter-bootstrap</code> tag at <a href="http://stackoverflow.com/questions/tagged/twitter-bootstrap">Stackoverflow</a> is a good place to find answers.</p>
-              
-              <hr>
-              <form>
-              <div class="input-group">
-                <div class="input-group-btn">
-                <button class="btn btn-default">+1</button><button class="btn btn-default"><i class="glyphicon glyphicon-share"></i></button>
+        <a href="#">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="#" class="pull-right">Catégorie</a> <h4>Nom de l'article</h4>
                 </div>
-                <input type="text" class="form-control" placeholder="Add a comment..">
-              </div>
-              </form>
-              
-            </div>
-         </div>
- 
-        <div class="panel panel-default">
-          <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Top Items</h4></div>
-            <div class="panel-body">
-              <div class="list-group">
-                <a href="http://bootply.com/tagged/bootstrap-3" class="list-group-item active">Bootstrap 3</a>
-                <a href="http://bootply.com/tagged/snippets" class="list-group-item">Snippets</a>
-                <a href="http://bootply.com/tagged/example" class="list-group-item">Examples</a>
-              </div>
-            </div>
-        </div>
-      
-    </div>
-  </div><!--/row-->
-  
-  <hr>
-  
-  <div class="row">
-    <div class="col-md-12"><h2>Posts</h2></div>
-    <div class="col-md-4 col-sm-6">
-        <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Upgrade to Bootstrap 3</h4></div>
-            <div class="panel-body">
-              <img src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=70" class="img-circle pull-right"> <a href="#">Guidance and Tools</a>
-              <div class="clearfix"></div>
-              <hr>
-              <p>Migrating from Bootstrap 2.x to 3 is not a simple matter of swapping out the JS and CSS files.
-              Bootstrap 3 is a major overhaul, and there are a lot of changes from Bootstrap 2.x. <a href="http://bootply.com/bootstrap-3-migration-guide">This guidance</a> is intended to help 2.x developers transition to 3.
-              </p>
-              <h5><a href="http://google.com/+bootply">More on Upgrading from +Bootply</a></h5>
-              
-            </div>
-         </div> 
-    </div>
-     <div class="col-md-4 col-sm-6">
-        <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Profiles</h4></div>
-            <div class="panel-body">
-              Check out some of our member profiles..
-              <hr>
-              <div class="well well-sm">
-                <div class="media">
-                    <a class="thumbnail pull-left" href="#">
-                        <img class="media-object" src="//placehold.it/80">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">John Doe</h4>
-                        <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span></p>
-                        <p>
-                           <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span> Message</a>
-                           <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-heart"></span> Favorite</a>
-                        </p>
-                    </div>
+                <div class="panel-body">
+                    <img src="http://www.fid-jaques.ch/wp_2014/wp-content/uploads/2014/04/Icon_WriteArticles-1.png" class="img-circle img-responsive" alt="image du produit">
+                    <center class="panel-content">
+                        Surmontant la crainte que le débit de la parole est inutile. Pourvu que ce soit un motif pour le détenir. Oubliés pendant près de cinq cents francs d'appointements et recevant de temps en temps elle prenait des épingles sur la pelote inépuisable de son coeur. Crois-moi, ma pauvre amie et vos vengeances cruelles pour moi, de mon côté. Longtemps simple commissionnaire, il n'oublierait point le chemin d'une caravane. Écoute-moi, chien, dit-elle au docteur, son petit flacon d'une femme dégage de la lumière que nous avions, mon frère... Retire-toi, monstre ; celui-ci ne lui laissa pas le temps bien ou mal avec son caractère ? Interprétée ainsi, non seulement le bétail et autres animaux sculptés sur les plus éloignés : ne ménage point ton prochain ! 
+Fous le camp, surpris par un détachement de chaque chose monte toujours en raison des services qu'on en fasse, de véritable société, on y faisait de la peine aussi, une passion. Où avez-vous pris cette affreuse toux, ma chère ; je vous connais. Six mille cinq cents francs. Usons de sévérité, non plus dans ses pensées. Téméraire, mais possible quand même, il pourrait bien avoir quelque peu malmené leurs descendants... Jonglant avec les actions, qui iront à ses enfants une seule larme de clarté dans le corridor et parvint à l'entraîner. Certes je m'efforçais le plus possible ; j'ai donc pensé que ce serait difficile à expliquer. Conventions, oui ; le connais-tu ?
+                    </center>
                 </div>
-               </div>
+                <div class="panel-price pull-right">
+                    <p>
+                        0,00€
+                    </p>
+                </div>
             </div>
-         </div> 
+        </a>
     </div>
-     <div class="col-md-4 col-sm-6">
-        <div class="panel panel-default">
-           <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>Inspiration</h4></div>
-            <div class="panel-body">
-              <img src="//placehold.it/150" class="img-circle pull-right"> <a href="#">Articles</a>
-              <div class="clearfix"></div>
-              <hr>
-              <div class="clearfix"></div>
-              <img src="http://placehold.it/120x90/3333CC/FFF" class="img-responsive img-thumbnail pull-right">
-              <p>The more powerful (and 100% fluid) Bootstrap 3 grid now comes in 4 sizes (or "breakpoints"). Tiny (for smartphones), Small (for tablets), Medium (for laptops) and Large (for laptops/desktops).</p>
-              <div class="clearfix"></div>
-              <hr>
-              <div class="clearfix"></div>
-              <img src="http://placehold.it/120x90/33CC33/FFF" class="img-responsive img-thumbnail pull-left" style="margin-right:5px;">
-              <p>Mobile first" is a responsive Web design practice that prioritizes consideration of smart phones and mobile devices when creating Web pages.</p>
-              
-              
-            </div>
-         </div> 
-    </div><!--/articles-->
-  </div>
-    
-  <hr>
-  
-  <div class="row">
-     <div class="col-sm-4 col-xs-6">
-      <div class="panel panel-default">
-        <div class="panel-thumbnail"><img src="//placehold.it/450X300/DD3333/EE3333" class="img-responsive"></div>
-        <div class="panel-body">
-          <p class="lead">Hacker News</p>
-          <p>120k Followers, 900 Posts</p>
-          
-          <p>
-            <img src="http://api.randomuser.me/portraits/med/men/20.jpg" width="28px" height="28px">
-            <img src="http://api.randomuser.me/portraits/med/men/21.jpg" width="28px" height="28px">
-            <img src="http://api.randomuser.me/portraits/med/men/14.jpg" width="28px" height="28px">
-          </p>
-        </div>
-      </div>
-    </div><!--/col-->
-    
-    <div class="col-sm-4 col-xs-6">
-      <div class="panel panel-default">
-        <div class="panel-thumbnail"><img src="//placehold.it/450X300/DD66DD/EE77EE" class="img-responsive"></div>
-        <div class="panel-body">
-          <p class="lead">Bootstrap Templates</p>
-          <p>902 Followers, 88 Posts</p>
-          
-          <p>
-            <img src="http://api.randomuser.me/portraits/med/men/4.jpg" width="28px" height="28px">
-            <img src="http://api.randomuser.me/portraits/med/men/24.jpg" width="28px" height="28px">
-          </p>
-        </div>
-      </div>
-    </div><!--/col-->
-    
-    <div class="col-sm-4 col-xs-6">
-      <div class="panel panel-default">
-        <div class="panel-thumbnail"><img src="//placehold.it/450X300/2222DD/2222EE" class="img-responsive"></div>
-        <div class="panel-body">
-          <p class="lead">Social Media</p>
-          <p>19k Followers, 789 Posts</p>
-          
-          <p>
-            <img src="http://api.randomuser.me/portraits/med/women/4.jpg" height="28px">
-            <img src="http://api.randomuser.me/portraits/med/men/4.jpg" width="28px" height="28px">
-          </p>
-        </div>
-      </div>
-    </div><!--/col-->
-   
-  </div>
+
+    <?php
+    }
+    ?>
+
 </div>
 </div>
