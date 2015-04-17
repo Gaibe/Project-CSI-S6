df5f72675597e85256f830b4ba3025e532d51c65
sofiane <sofiane@10.10.125.92>
2015-04-18 00:15:06 +0200

    Formulaire ajout réduction

--- admin/ajout-reduction.php
+++ admin/ajout-reduction.php
@@ -1,7 +1,6 @@
 <?php
 include_once("../header.php");
 
-
 if (isset($_SESSION['admin']) === false) {
     $project_name = explode("/", $_SERVER["PHP_SELF"])[1];
     header("Location: /$project_name/");
@@ -10,9 +9,11 @@ if (isset($_SESSION['admin']) === false) {
 else {
     include_once("../header.php");
     include_once("../modele/Produit.php");
+    include_once("../modele/Client.php");
     $list_prod = Produit::findAll();
+    $list_client = Client::findAll();
+    
 ?>
-
 <div class="container" id="main">
 <div class="row">
     <div class="col-md-offset-2" ng-app="sample">
@@ -20,34 +21,49 @@ else {
             <h2 class="col-md-offset-3 col-md-8">Ajout d'une réduction</h2><br>
 
             <div class="form-group">
-                <label class="col-md-3 control-label" for="Nom">Montant réduction (en %)</label>
+                <label class="col-md-3 control-label" for="Montant">Montant réduction (en %)</label>
                 <div class="col-md-4">
                     <input id="Montant-reduction" type="text" class="form-control" name="Montant" ng-model="Montant" />
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="col-md-3 control-label" for="Description">Nombre de produits avant application</label>
+                <label class="col-md-3 control-label" for="NbProduit">Nombre de produits avant application</label>
                 <div class="col-md-4">
                     <input id="Nb-produits" type="text" class="form-control" name="NbProduit" ng-model="NbProduit" />
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="col-md-3 control-label" for="Image-produit">Date de fin</label>
+                <label class="col-md-3 control-label" for="DateFin">Date de fin</label>
                 <div class="col-md-4">
                     <input id="Date-fin" type="text" class="form-control" name="DateFin" ng-model="DateFin" />
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="col-md-3 control-label" for="Categorie">Produits</label>
+                <label class="col-md-3 control-label" for="Produits">Produits</label>
                 <div class="col-md-4">
-                    <select multiple class="form-control" name="Produits">
+                    <select multiple class="form-control" name="Produits[]">
                         <?php
                         foreach ($list_prod as $prod) {
                         ?>
-                        <option value="<?php echo $prod['id_produit']; ?>"><?php echo $categ['libelle']; ?></option>
+                        <option value="<?php echo $prod['id_produit']; ?>"><?php echo $prod['libelle']; ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="Clients">Clients</label>
+                <div class="col-md-4">
+                    <select multiple class="form-control" name="Clients[]">
+                        <?php
+                        foreach ($list_client as $client) {
+                        ?>
+                        <option value="<?php echo $client['id_client']; ?>"><?php echo $client['nom']; ?></option>
                         <?php
                         }
                         ?>
