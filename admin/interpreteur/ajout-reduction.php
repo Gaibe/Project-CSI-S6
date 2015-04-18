65453678e8a34b6acc4d0020274347e19c8ec80a
sofiane <sofiane@10.10.125.92>
2015-04-19 00:15:03 +0200

    Traitement du formulaire d'ajout de réduction

--- admin/interpreteur/ajout-reduction.php
+++ admin/interpreteur/ajout-reduction.php
@@ -4,7 +4,7 @@ require_once("../../base.php");
 require_once("../../modele/Hydrator.php");
 require_once("../../modele/Reduction.php");
 require_once("../../modele/Client.php");
-
+$connection = base::getConnection();
 
 if (isset($_POST['Montant']) === true && isset($_POST['NbProduit']) === true 
     && isset($_POST['DateFin']) === true){
@@ -15,46 +15,42 @@ if (isset($_POST['Montant']) === true && isset($_POST['NbProduit']) === true
     $reduction->__set('nb_produit', $_POST['NbProduit']);
     $reduction->__set('date_fin', $_POST['DateFin']);
     $reduction->insert();
-    
+    $idreduction = $connection->lastInsertId();
     
     if (isset($_POST['Produits']) && !empty($_POST['Produits'])){
         $connection = base::getConnection();
         $list_prod = $_POST['Produits'];
         foreach($list_prod as $prod){
-            $stmt = $connection->prepare("INSERT INTO reduction_has_produit (reduction_id_reduction, produit_id_produit) VALUES (:reduction_id_reduction, :produit_id_produit)");
-            $stmt->bindParam(':reduction_id_reduction', $reduction->__get('id_reduction'));
-            $stmt->bindParam(':produit_id_produit', $prod);
-            $stmt->execute();
+            Reduction::insertReductionHasProduit($idreduction, $prod);
         }
     
     }
     
     if (isset($_POST['Clients']) && !empty($_POST['Clients'])){
+        echo "ok2";
         $connection = base::getConnection();
         $list_client = $_POST['Clients'];
         foreach($list_client as $client){
-            $stmt = $connection->prepare("INSERT INTO reduction_has_client (reduction_id_reduction, client_id_client) VALUES (:reduction_id_reduction, :client_id_client)");
-            $stmt->bindParam(':reduction_id_reduction', $reduction->__get('id_reduction'));
-            $stmt->bindParam(':client_id_client', $client);
-            $stmt->execute();
+            Reduction::insertReductionHasClient($idreduction, $client);
         }
     
     }
     
-    else if (isset($_POST['Clients']) && empty($_POST['Clients'])){
+    else{
         $connection = base::getConnection();
         $list_client = Client::findAll();
         foreach($list_client as $client){
-            $stmt = $connection->prepare("INSERT INTO reduction_has_client (reduction_id_reduction, client_id_client) VALUES (:reduction_id_reduction, :client_id_client)");
-            $stmt->bindParam(':reduction_id_reduction', $reduction->__get('id_reduction'));
-            $stmt->bindParam(':client_id_client', $client->__get('id_client'));
-            $stmt->execute();
+            $idclient = $client["id_client"];
+            Reduction::insertReductionHasClient($idreduction, $idclient);
         }
     }
+     
+     
 }
 
-
+/*
 header("Location: ../../");
 exit;
+ */
  
 
