81cebecf3beeb64f9e41f150c786c99cdcb60367
Victorien Schweitzer <gaibe--ct--@live.fr>
2015-04-18 15:50:59 +0200

    Ajout de sécurité grace aux index

---
+++ fonts/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>404 Not Found</title>
+    </head>
+    <body>
+        <h1>Not Found</h1>
+        <p>The requested URL <?php echo dirname($_SERVER["PHP_SELF"]); ?> was not found on this server.</p>
+        </br>
+        <address>Apache/2.4.9 PHP/5.5.12 Server at localhost Port 80</address>
+    </body>
+</html>
