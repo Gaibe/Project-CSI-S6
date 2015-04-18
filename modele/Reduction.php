3b3c2de4dc1cfc98a7784b2fd1f374d8431704c9
sofiane <sofiane@10.10.125.92>
2015-04-19 00:15:54 +0200

    Ajout des méthodes pour les insertions dans les tables reduction_has_prduit et reduction_has_client

--- modele/Reduction.php
+++ modele/Reduction.php
@@ -6,7 +6,7 @@ final class Reduction {
 
     protected $montant_reduction;
     
-    protected $nb_produit;
+    protected $nombre_produit;
     
     protected $date_debut;
     
@@ -48,13 +48,29 @@ final class Reduction {
     
     public function insert() {
         $connection = base::getConnection();
-        $stmt = $connection->prepare("INSERT INTO reduction (montant_reduction, nb_produit, date_fin) 
+        $stmt = $connection->prepare("INSERT INTO reduction (montant_reduction, nombre_produit, date_fin) 
             VALUES (:montant_reduction, :nb_produit, :date_fin)");
         $stmt->bindParam(':montant_reduction', $this->montant_reduction);
         $stmt->bindParam(':nb_produit', $this->nb_produit);
         $stmt->bindParam(':date_fin', $this->date_fin);
         $stmt->execute();
         
+    }
+    
+    public static function insertReductionHasProduit($idreduction, $prod) {
+        $connection = base::getConnection();
+        $stmt = $connection->prepare("INSERT INTO reduction_has_produit (reduction_id_reduction, produit_id_produit) VALUES (:reduction_id_reduction, :produit_id_produit)");
+        $stmt->bindParam(':reduction_id_reduction', $idreduction);
+        $stmt->bindParam(':produit_id_produit', $prod);
+        $stmt->execute();
+    }
+    
+    public static function insertReductionHasClient($idreduction, $client) {
+        $connection = base::getConnection();
+        $stmt = $connection->prepare("INSERT INTO reduction_has_client (reduction_id_reduction, client_id_client) VALUES (:reduction_id_reduction, :client_id_client)");
+        $stmt->bindParam(':reduction_id_reduction', $idreduction);
+        $stmt->bindParam(':client_id_client', $client);
+        $stmt->execute();
     }
     
 }
