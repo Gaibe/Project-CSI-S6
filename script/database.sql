2fb07f771cf62ba1106b7bd5ebdafa765ffd5d46
Victorien Schweitzer <gaibe--ct--@live.fr>
2015-04-28 18:44:46 +0200

    Trigger dans le meme fichier que database

--- script/database.sql
+++ script/database.sql
@@ -249,6 +249,37 @@ CREATE TABLE IF NOT EXISTS `projet_csi`.`reduction_has_client` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+
+delimiter //
+
+CREATE TRIGGER panier_has_produit_delete_t
+AFTER DELETE ON panier_has_produit
+FOR EACH ROW
+BEGIN
+    IF (SELECT COUNT(*)
+        FROM panier_has_produit
+        WHERE panier_id_panier = OLD.panier_id_panier) = 0 
+    THEN
+
+        UPDATE panier SET prix_total = 0.00, quantite_totale = 0
+        WHERE id_panier = OLD.panier_id_panier;
+    END IF;
+END//
+
+
+
+delimiter ;
+
+
+
+
+-- INSERTION DE DONNEES
+
+
+
+
+
 INSERT INTO `projet_csi`.`client` (`nom`, `prenom`, `email`, `pseudo`, `mot_passe`, `role`)
 VALUES ('projet', 'admin', 'admin@csi.com', 'projet_csi', '3cfe9d1883c5819dd5fdcc3c57fa7a892db0c56c', 'admin');
 
